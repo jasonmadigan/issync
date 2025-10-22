22fed8c2491bac752dfa99ec4a8e2dbc4e7a76a7
Jason Madigan <jason@jasonmadigan.com>
2025-10-22 15:54:12 +0100

    bump version to 0.2.0

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name('issync')
   .description('bidirectional github issue syncing')
-  .version('0.1.0');
+  .version('0.2.0');
 
 program
   .command('down')
